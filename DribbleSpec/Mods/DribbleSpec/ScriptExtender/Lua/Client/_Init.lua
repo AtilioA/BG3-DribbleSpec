22c902afae57928a66ba72ab2fb30a5f64a8ecb9
Atílio <27153776+AtilioA@users.noreply.github.com>
2026-03-02 21:33:05 -0300

    chore: update unused stuff

--- DribbleSpec/Mods/DribbleSpec/ScriptExtender/Lua/Client/_Init.lua
+++ DribbleSpec/Mods/DribbleSpec/ScriptExtender/Lua/Client/_Init.lua
@@ -1,10 +1,10 @@
-local MODVERSION = Ext.Mod.GetMod(ModuleUUID).Info.ModVersion
+-- local MODVERSION = Ext.Mod.GetMod(ModuleUUID).Info.ModVersion
 
-if MODVERSION == nil then
-    DSPrint(0, "DribbleSpec loaded (version unknown)")
-else
-    table.remove(MODVERSION)
+-- if MODVERSION == nil then
+--     DSPrint(0, "DribbleSpec loaded (version unknown)")
+-- else
+--     table.remove(MODVERSION)
 
-    local versionNumber = table.concat(MODVERSION, ".")
-    DSPrint(0, "DribbleSpec (client) version " .. versionNumber .. " loaded")
-end
+--     local versionNumber = table.concat(MODVERSION, ".")
+--     DSPrint(0, "DribbleSpec (client) version " .. versionNumber .. " loaded")
+-- end
